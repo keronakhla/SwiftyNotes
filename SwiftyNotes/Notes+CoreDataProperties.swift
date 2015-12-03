3a9b8e79ef59f671bdc8f6219a9e96e2ad63389d
Kerolos <keronakhla@gmail.com>
2015-12-03 15:31:30 -0800

    Subclass NSManagedObject

---
+++ SwiftyNotes/Notes+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  Notes+CoreDataProperties.swift
+//  SwiftyNotes
+//
+//  Created by Kerolos Nakhla on 12/3/15.
+//  Copyright © 2015 Kerolos Nakhla. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Notes {
+
+    @NSManaged var timeStamp: NSDate?
+    @NSManaged var body: String?
+
+}
---
+++ SwiftyNotes/Notes.swift
@@ -0,0 +1,17 @@
+//
+//  Notes.swift
+//  SwiftyNotes
+//
+//  Created by Kerolos Nakhla on 12/3/15.
+//  Copyright © 2015 Kerolos Nakhla. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Notes: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
